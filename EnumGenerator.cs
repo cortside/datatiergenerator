4bb37d255bca89e45592887f037856a0d8255511
cort <none@none>
2002-04-19 14:28:03 +0000

    support enum generation

---
+++ EnumGenerator.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Data;
+using System.IO;
+using System.Collections;
+using System.Text;
+
+namespace Spring2.DataTierGenerator {
+
+    public class EnumGenerator : GeneratorBase {
+	private EnumType type;
+
+	public EnumGenerator(Configuration options, EnumType type) : base(options, null) {
+	    this.type = type;
+	}
+
+	public void Generate() {
+	    StringBuilder sb = new StringBuilder();
+
+	    sb.Append("using System;\n");
+	    sb.Append("using System.Collections;\n");
+	    sb.Append("\n");
+	    sb.Append("namespace ").Append(options.GetTypeNameSpace(type.Name)).Append(" {\n");
+	    sb.Append("\n");
+	    sb.Append("\tpublic class ").Append(type.Name).Append(" : Spring2.Core.Types.EnumDataType	{\n");
+	    sb.Append("\t\n");
+	    sb.Append("\t\tprivate static readonly IList OPTIONS = new ArrayList();\n");
+	    sb.Append("\t\t\n");
+	    sb.Append("\t\tpublic static readonly new ").Append(type.Name).Append(" DEFAULT = new ").Append(type.Name).Append("();\n");
+	    sb.Append("\t\tpublic static readonly new ").Append(type.Name).Append(" UNSET = new ").Append(type.Name).Append("();\n");
+	    sb.Append("\t\t\n");
+
+	    foreach (EnumValue value in type.Values) {
+		sb.Append("\t\tpublic static readonly ").Append(type.Name).Append(" ").Append(value.Name.ToUpper()).Append(" = new ").Append(type.Name).Append("(\"").Append(value.Code).Append("\", \"").Append(value.Name).Append("\");\n");
+	    }
+
+	    sb.Append("\t\t\n");
+	    sb.Append("\t\tpublic static ").Append(type.Name).Append(" GetInstance(Object value) {\n");
+	    sb.Append("\t\t\tif (value is String) {\n");
+	    sb.Append("\t\t\t\tforeach (").Append(type.Name).Append(" t in OPTIONS) {\n");
+	    sb.Append("\t\t\t\t\tif (t.Value.Equals(value)) {\n");
+	    sb.Append("\t\t\t\t\t\treturn t;\n");
+	    sb.Append("\t\t\t\t\t}\n");
+	    sb.Append("\t\t\t\t}\n");
+	    sb.Append("\t\t\t}\n");
+	    sb.Append("\t\t\t\n");
+	    sb.Append("\t\t\treturn UNSET;\n");
+	    sb.Append("\t\t}\n");
+	    sb.Append("\t\t\n");
+	    sb.Append("\t\tprivate ").Append(type.Name).Append("() {\n");
+	    sb.Append("\t\t}\n");
+	    sb.Append("\t\t\n");
+	    sb.Append("\t\tprivate ").Append(type.Name).Append("(String code, String name) {\n");
+	    sb.Append("\t\t\tthis.code = code;\n");
+	    sb.Append("\t\t\tthis.name = name;\n");
+	    sb.Append("\t\t\tOPTIONS.Add(this);\n");
+	    sb.Append("\t\t}\n");
+	    sb.Append("\t\t\n");
+	    sb.Append("\t\tpublic override Boolean IsDefault {\n");
+	    sb.Append("\t\t\tget { return Object.ReferenceEquals(this, DEFAULT); }\n");
+	    sb.Append("\t\t}\n");
+	    sb.Append("\t\t\n");
+	    sb.Append("\t\tpublic override Boolean IsUnset {\n");
+	    sb.Append("\t\t\tget { return Object.ReferenceEquals(this, UNSET); }\n");
+	    sb.Append("\t\t}\n");
+	    sb.Append("\t}\n");
+	    sb.Append("}\n");
+
+	    WriteToFile(options.RootDirectory + options.TypesClassDirectory + "\\" + options.GetTypeClassName(type.Name) + ".cs", sb.ToString(), false);
+	}
+
+    }
+}
---
+++ EnumValue.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Text;
+using System.Xml;
+
+namespace Spring2.DataTierGenerator {
+    public class EnumValue : Spring2.Core.DataObject.DataObject {
+
+	protected String name = String.Empty;
+	protected String code = String.Empty;
+
+	public String Name {
+	    get { return this.name; }
+	    set { this.name = value; }
+	}
+
+	public String Code {
+	    get { return this.code; }
+	    set { this.code = value; }
+	}
+
+	public String ToXml() {
+	    StringBuilder sb = new StringBuilder();
+	    sb.Append("<value name=\"").Append(name).Append("\"");
+	    sb.Append(" code=\"").Append(code).Append("\"");
+	    sb.Append(" />");
+
+	    return sb.ToString();
+	}
+
+	public static ArrayList ParseFromXml(String name, Configuration options, XmlDocument doc, Hashtable sqltypes, Hashtable types) {
+	    ArrayList values = new ArrayList();
+	    XmlNodeList elements = doc.DocumentElement.GetElementsByTagName("enum");
+	    foreach (XmlNode element in elements) {
+		if (element.Attributes["name"].Value.Equals(name) && element.HasChildNodes) {
+		    foreach (XmlNode node in element.ChildNodes) {
+			if (node.Name.Equals("value")) {
+			    EnumValue value = new EnumValue();
+			    value.Name = node.Attributes["name"].Value;
+			    value.Code = node.Attributes["code"].Value;
+			    values.Add(value);
+			}
+		    }
+		}
+	    }
+	    return values;
+	}
+
+    }
+}
