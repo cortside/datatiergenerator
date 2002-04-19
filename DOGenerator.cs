ce1388a4beb765e9614cf2290da9a6c219922fa8
crandall <none@none>
2002-04-19 08:05:32 +0000

    Change indentation to four spaces from a tab character

--- DOGenerator.cs
+++ DOGenerator.cs
@@ -19,12 +19,12 @@ namespace Spring2.DataTierGenerator {
 
 	    // class definition
 	    sb.Append("namespace " + options.GetDONameSpace(entity.Name) + " {\n");
-	    sb.Append("\tpublic class " + options.GetDOClassName(entity.Name) + " : Spring2.Core.DataObject.DataObject {\n\n");
+	    sb.Append("    public class " + options.GetDOClassName(entity.Name) + " : Spring2.Core.DataObject.DataObject {\n\n");
 
 	    // declaration of private member variables
 	    foreach (Field field in entity.Fields) {
 		if (field.Name.IndexOf('.')<0) {
-		    sb.Append("\t\t").Append(field.AccessModifier).Append(" ");
+		    sb.Append("\t").Append(field.AccessModifier).Append(" ");
 		    sb.Append(field.Type.Name);
 		    sb.Append(" ").Append(field.GetFieldFormat()).Append(" = ");
 		    sb.Append(field.Type.NewInstanceFormat).Append(";\n");
@@ -36,22 +36,22 @@ namespace Spring2.DataTierGenerator {
 	    foreach (Field field in entity.Fields) {
 		if (field.Name.IndexOf('.')<0) {
 		    if (field.Description.Length>0) {
-			sb.Append("\t\t/// <summary>\n");
-			sb.Append("\t\t/// ").Append(field.Description).Append("\n");
-			sb.Append("\t\t/// </summary>\n");
+			sb.Append("\t/// <summary>\n");
+			sb.Append("\t/// ").Append(field.Description).Append("\n");
+			sb.Append("\t/// </summary>\n");
 		    }
 
-		    sb.Append("\t\tpublic ");
+		    sb.Append("\tpublic ");
 		    sb.Append(field.Type.Name);
 		    sb.Append(" ").Append(field.GetMethodFormat()).Append(" {\n");
-		    sb.Append("\t\t\tget { return this.").Append(field.GetFieldFormat()).Append("; }\n");
-		    sb.Append("\t\t\tset { this.").Append(field.GetFieldFormat()).Append(" = value; }\n");
-		    sb.Append("\t\t}\n\n");
+		    sb.Append("\t    get { return this.").Append(field.GetFieldFormat()).Append("; }\n");
+		    sb.Append("\t    set { this.").Append(field.GetFieldFormat()).Append(" = value; }\n");
+		    sb.Append("\t}\n\n");
 		}
 	    }
 		
 	    // Close out the class and namespace
-	    sb.Append("\t}\n");
+	    sb.Append("    }\n");
 	    sb.Append("}\n");
 
 	    WriteToFile(options.RootDirectory + options.DoClassDirectory + "\\" + options.GetDOClassName(entity.Name) + ".cs", sb.ToString(), false);
