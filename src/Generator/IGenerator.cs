cce4ef0fe2c147fcbb57ca2f4f68721ef6b67411
cort <none@none>
2002-06-17 15:07:21 +0000

    interface that all generatators are expected to support

---
+++ src/Generator/IGenerator.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Spring2.DataTierGenerator.Generator {
+    /// <summary>
+    /// Summary description for IGenerator.
+    /// </summary>
+    public interface IGenerator {
+
+	/// <summary>
+	/// Call the generator to action and output somthing.
+	/// </summary>
+	void Generate();
+
+    }
+}
