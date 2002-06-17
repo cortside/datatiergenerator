652be7875e5f9de5ee32abdc30d7fc1aad7be6c7
cort <none@none>
2002-06-17 14:50:40 +0000

    validation delegate for parsers to pass to elements for returning parsing errors

---
+++ src/Parser/ParserValidationDelegate.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Spring2.DataTierGenerator.Parser
+{
+	/// <summary>
+	/// Summary description for ParserValidationDelegate.
+	/// </summary>
+	public delegate void ParserValidationDelegate(ParserValidationArgs args);
+}
