3cddeac43b9a96be04bb415a0c1d04318c04f36a
cort <none@none>
2002-06-17 14:49:32 +0000

    interface that all parsers are to support - parsers are passed around by this interface

---
+++ src/Parser/IParser.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+
+
+namespace Spring2.DataTierGenerator.Parser {
+    /// <summary>
+    /// Summary description for IParser.
+    /// </summary>
+    public interface IParser {
+
+	Configuration Configuration {
+	    get;
+	}
+
+	Boolean IsValid {
+	    get;
+	}
+
+	IList Errors {
+	    get;
+	}
+
+	String ErrorDescription {
+	    get;
+	}
+
+	IList Databases {
+	    get;
+	}
+
+	IList Entities {
+	    get;
+	}
+
+	IList Enums {
+	    get;
+	}
+
+	IList Collections {
+	    get;
+	}
+
+	ICollection Types {
+	    get;
+	}
+
+	ICollection SqlTypes {
+	    get;
+	}
+    }
+}
