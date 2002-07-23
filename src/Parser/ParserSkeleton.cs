0f14cd9b891f6cd7b73a0f0335756570b8a32b8d
cort <none@none>
2002-07-23 17:08:34 +0000

    support IParser changes move validation methods from XmlParser to this base class

--- src/Parser/ParserSkeleton.cs
+++ src/Parser/ParserSkeleton.cs
@@ -27,6 +27,7 @@ namespace Spring2.DataTierGenerator.Parser {
 	protected Hashtable types = new Hashtable();
 	protected Hashtable sqltypes = new Hashtable();
 	protected Element.Generator generator = new Element.Generator();
+	protected Element.Parser parser = new Element.Parser();
 
 	protected Boolean isValid = false;
 	protected IList errors = new ArrayList();
@@ -80,6 +81,48 @@ namespace Spring2.DataTierGenerator.Parser {
 	public Element.Generator Generator {
 	    get { return generator; }
 	}
+
+	public Element.Parser Parser {
+	    get { return parser; }
+	}
+
+	/// <summary>
+	/// Event handler for parser validation events
+	/// </summary>
+	/// <param name="args"></param>
+	protected void ParserValidationEventHandler(ParserValidationArgs args) {
+	    if (args.Severity.Equals(ParserValidationSeverity.ERROR)) {
+		isValid = false;
+	    }
+	    errors.Add(args);
+	}
+
+
+	/// <summary>
+	/// Post-parse validations
+	/// </summary>
+	/// <param name="vd"></param>
+	protected void Validate(ParserValidationDelegate vd) {
+	    //TODO: walk through collection to make sure that cross relations are correct.
+
+	    foreach(Entity entity in entities) {
+		if (entity.SqlEntity.Name.Length>0 && !entity.SqlEntity.HasUpdatableColumns()) {
+		    vd(ParserValidationArgs.NewWarning("entity " + entity.Name + " does not have any editable fields and does not have x specified.  No update stored procedures or update DAO methods will be generated."));
+		}
+	    }
+
+	    // make sure that all columns are represented in entities
+	    foreach(Entity entity in entities) {
+		if (entity.SqlEntity.Name.Length>0) {
+		    foreach(Column column in entity.SqlEntity.Columns) {
+			if (entity.FindFieldByColumnName(column.Name)==null) {
+			    vd(ParserValidationArgs.NewError("could not find field represending column " + column.Name + " in entity " + entity.Name + "."));
+			}
+		    }
+		}
+	    }
+	}
+
 
     }
 }
