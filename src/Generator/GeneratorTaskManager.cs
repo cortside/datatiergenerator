a04322eb87cd22081bd77d24cd0a2f79e9687dae
cort <none@none>
2002-10-10 23:37:42 +0000

    show warnings

--- src/Generator/GeneratorTaskManager.cs
+++ src/Generator/GeneratorTaskManager.cs
@@ -26,10 +26,15 @@ namespace Spring2.DataTierGenerator.Generator {
 	}
 
 	public override void Generate() {
-	    Console.Out.Write("\n");
-	    Console.Out.WriteLine(String.Empty.PadLeft(20,'='));
+	    //Console.Out.Write("\n");
+	    //Console.Out.WriteLine(String.Empty.PadLeft(20,'='));
 
 	    if (parser.IsValid) {
+
+		if (parser.HasWarnings) {
+		    Console.Out.WriteLine("The parser is in a valid state, but reported the following issues:\n" + parser.ErrorDescription);
+		}
+
 		IList tasks = parser.Generator.FindTasksByElement("sqlentity");
 		if (tasks.Count > 0) {
 		    foreach (Database database in parser.Databases) {
@@ -91,10 +96,10 @@ namespace Spring2.DataTierGenerator.Generator {
 		}
 
 	    } else {
-		Console.Out.WriteLine("Parser was not in a valid state and reported the following errors:\n" + parser.Errors);
+		Console.Out.WriteLine("Parser was not in a valid state and reported the following errors:\n" + parser.ErrorDescription);
 	    }
 			
-	    Console.Out.WriteLine(String.Empty.PadLeft(20,'='));
+	    //Console.Out.WriteLine(String.Empty.PadLeft(20,'='));
 	}
 
     }
