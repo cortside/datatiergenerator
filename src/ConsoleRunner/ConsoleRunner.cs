4c8000376c1a19dc52b341e7482fd22a67147f63
cort <none@none>
2003-04-07 21:45:51 +0000

    add console-based runner remove DTGEditor from solution (keeping until I can decide if there will ever be one in the future)

---
+++ src/ConsoleRunner/ConsoleRunner.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections;
+using System.IO;
+using System.Xml;
+using System.Xml.Schema;
+using System.Reflection;
+
+using Spring2.Core.Xml;
+
+using Spring2.DataTierGenerator.Generator;
+
+namespace Spring2.DataTierGenerator.ConsoleRunner {
+
+    /// <summary>
+    /// Console runner
+    /// </summary>
+    class ConsoleRunner {
+
+	/// <summary>
+	/// Assembly entry point
+	/// </summary>
+	[STAThread]
+	static void Main(string[] args) {
+	    if (args.Length==1) {
+		Generate(args[0]);
+	    } else {
+		Console.Out.WriteLine("usage: DataTierGenerator.exe <xml config filename>");
+	    }
+	}
+
+	private static void Generate(String filename) {
+	    FileInfo file = new FileInfo(filename);
+	    if (!file.Exists) {
+		Console.Out.WriteLine("could not find config file: " + file.FullName);
+		return;
+	    }
+	    try {
+		Console.Out.WriteLine(String.Empty.PadLeft(20,'='));
+		Console.Out.WriteLine("Start :: " + DateTime.Now.ToString());
+		Console.Out.WriteLine(String.Empty.PadLeft(20,'='));
+
+		XmlDocument doc = new XmlDocument();
+		// while this might seem silly, extended ASCII chararcter encoding does not happen if just the filename 
+		// is passed to the Load method
+		StreamReader reader = file.OpenText();
+		doc.Load(reader);
+		reader.Close();
+
+		XmlNode pi = GetProcessingInstruction(doc, "dtg");
+		String parserClassname = GetProcessingInstructionAttribute(pi, "parser");
+
+		// instantiate the parser class
+		System.Type clazz = System.Type.GetType(parserClassname, true);
+		Object o = System.Activator.CreateInstance(clazz);
+		if (o is IParser) {
+		    IParser parser = (IParser) o;
+		    parser.Parse(filename);
+
+		    if (parser.IsValid) {
+			IGenerator generator = null;
+			try {
+			    // locate and instanciate the Generator class specified by the parser
+			    clazz = System.Type.GetType(parser.Generator, true);
+			    o = System.Activator.CreateInstance(clazz);
+			    if (o is IGenerator) {
+				generator = (IGenerator) o;
+			    } else  {
+				Console.Out.WriteLine("ERROR: class " + parser.Generator + " does not support IGenerator interface.\n");
+			    }
+			} catch (Exception ex) {
+			    Console.Out.WriteLine("ERROR: could not instanciate generator class " + parser.Generator + "\n" + ex);
+			}
+
+			// if the generator is not null, generate
+			if (generator != null) {
+			    generator.Generate(parser);
+			    foreach(String s in generator.Log) {
+				Console.Out.WriteLine(s);
+			    }
+			}
+		    } else {
+			Console.Out.WriteLine("ERROR: Parser found errors:");
+			Console.Out.WriteLine(parser.Log);
+		    }
+		} else  {
+		    Console.Out.WriteLine("ERROR: class " + parserClassname + " does not support IParser interface.\n");
+		}
+
+		Console.Out.WriteLine(String.Empty.PadLeft(20,'='));
+		Console.Out.WriteLine("Done :: " + DateTime.Now.ToString());
+		Console.Out.WriteLine(String.Empty.PadLeft(20,'='));
+	    } catch (Exception ex) {
+		Console.Out.WriteLine("An error occcurred while generating.\n\n" + ex.ToString());
+	    }
+	}
+
+	protected static XmlNode GetProcessingInstruction(XmlDocument doc, String pi) {
+	    foreach(XmlNode node in doc.ChildNodes) {
+		if (node.Name.Equals(pi)) {
+		    return node;
+		}
+	    }
+	    return null;
+	}
+
+	protected static String GetProcessingInstructionAttribute(XmlNode node, String attribute) {
+	    String s = node == null ? String.Empty : node.Value;
+	    if (s != null && s.IndexOf(attribute + "=\"") >= 0) { 
+		String value = s.Substring(s.IndexOf(attribute + "=\"") + attribute.Length + 2);
+		return value.Substring(0,value.IndexOf("\""));
+	    }
+
+	    return null;
+	}
+
+    }
+}
