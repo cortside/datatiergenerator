55137d75a175437422f828ce0173d19896a7648f
cort <none@none>
2003-06-21 01:02:19 +0000

    add Class as possible usage for Generate attribute (needed for inner classes)

--- src/Attribute/GenerateAttribute.cs
+++ src/Attribute/GenerateAttribute.cs
@@ -6,7 +6,7 @@ namespace Spring2.DataTierGenerator.Attribute {
     /// <summary>
     /// Indicates that a method or property is generated and contents can be replaced
     /// </summary>
-    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Constructor | AttributeTargets.Field, Inherited=false, AllowMultiple=false)]
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Constructor | AttributeTargets.Field | AttributeTargets.Class, Inherited=false, AllowMultiple=false)]
     public class GenerateAttribute : System.Attribute {
 
 	public GenerateAttribute() {            
