3b2d373e1a613ec70b025ffa896c9dc0ee158d23
cort <none@none>
2004-01-07 22:56:11 +0000

    updated version of NVelocity

---
+++ lib/copynvelocity.sh
@@ -0,0 +1,5 @@
+cp /work/SourceForge/NVelocity-clean/build/log4net.dll .
+cp /work/SourceForge/NVelocity-clean/build/Commons.dll .
+cp /work/SourceForge/NVelocity-clean/build/Commons.pdb .
+cp /work/SourceForge/NVelocity-clean/build/NVelocity.dll .
+cp /work/SourceForge/NVelocity-clean/build/NVelocity.pdb .
