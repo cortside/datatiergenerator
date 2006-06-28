d0ff0ca5cbc0fef9059767ee5b804171aadd9168
cort <none@none>
2006-06-28 12:47:35 +0000

    add support for detecting and updating both VS2003 and VS2005 project files

--- src/UpdateProjectFiles/UpdateProjectFiles.cs
+++ src/UpdateProjectFiles/UpdateProjectFiles.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Collections.Specialized;
 using System.IO;
 using System.Xml;
@@ -9,13 +8,14 @@ namespace Spring2.DataTierGenerator.UpdateProjectFiles {
     /// <summary>
     /// Updates a project with files not already included in project based on file pattern arguments
     /// </summary>
-    class UpdateProjectFiles {
-	/// <summary>
+    public class UpdateProjectFiles {
+	
+    	/// <summary>
 	/// The main entry point for the application.
 	/// </summary>
 	[STAThread]
 	static void Main(string[] args) {
-	    // assumes that the project is the first argument
+	    // assumes that the project filename is the first argument
 	    FileInfo project = new FileInfo(args[0]);
 	    if (!project.Exists) {
 		Console.Out.WriteLine("Project file could not be found: " + project.FullName);
@@ -25,7 +25,7 @@ namespace Spring2.DataTierGenerator.UpdateProjectFiles {
 	    // the rest of the arguments could be files or directories
 	    StringCollection files = new StringCollection();
 	    for(Int32 i=1; i < args.Length; i++) {
-	    	String[] filenames = Directory.GetFiles(Environment.CurrentDirectory, args[i]);
+		String[] filenames = Directory.GetFiles(Environment.CurrentDirectory, args[i]);
 		files.AddRange(filenames);
 	    }
 
@@ -36,7 +36,30 @@ namespace Spring2.DataTierGenerator.UpdateProjectFiles {
 	    XmlDocument doc = new XmlDocument();
 	    doc.Load(project.FullName);
 
+	    Boolean updated = false;
+	    if (doc.FirstChild.Name.Equals("VisualStudioProject")) {
+		updated = AddFilesVS2003(doc, files, project);
+	    } else if (doc.FirstChild.Name.Equals("Project")) {
+		updated = AddFilesVS2005(doc, files, project);
+	    } else {
+		Console.Out.WriteLine("ERROR: Unknown project file version");
+	    }
+
+	    if (updated) {
+		Console.Out.WriteLine("Saving " + project.Name);
+		doc.Save(project.FullName);
+	    } else {
+		Console.Out.WriteLine("no new files to add");
+	    }
+	}
+
+	private static bool AddFilesVS2003(XmlDocument doc, StringCollection files, FileInfo project) {
 	    XmlNode include = doc.SelectSingleNode("/VisualStudioProject/CSHARP/Files/Include");
+		
+	    if (include == null) {
+		Console.Out.WriteLine("ERROR: Include node not found");
+		return false;
+	    }
 
 	    Boolean updated = false;
 	    foreach(String filename in files) {
@@ -48,6 +71,12 @@ namespace Spring2.DataTierGenerator.UpdateProjectFiles {
 		XmlNode node = doc.SelectSingleNode(xpath);
 		if (node == null) {
 		    Console.Out.WriteLine("Adding " + path + " to " + project.FullName);
+
+		    //<File
+		    //    RelPath = "AssemblyVersionInfo.cs"
+		    //    SubType = "Code"
+		    //    BuildAction = "Compile"
+		    ///>
 
 		    XmlElement element = doc.CreateElement("File");
 		    XmlAttribute attribute = doc.CreateAttribute("RelPath");
@@ -66,14 +95,60 @@ namespace Spring2.DataTierGenerator.UpdateProjectFiles {
 		    updated = true;
 		}
 	    }
-
-	    if (updated) {
-		Console.Out.WriteLine("Saving " + project.Name);
-		doc.Save(project.FullName);
-	    } else {
-		Console.Out.WriteLine("no new files to add");
-	    }
+	    return updated;
 	}
+    	
+	private static bool AddFilesVS2005(XmlDocument doc, StringCollection files, FileInfo project) {
+	    //Instantiate an XmlNamespaceManager object. 
+	    XmlNamespaceManager xmlnsManager = new XmlNamespaceManager(doc.NameTable);
 
+	    //Add the namespaces used in books.xml to the XmlNamespaceManager.
+	    xmlnsManager.AddNamespace("default", "http://schemas.microsoft.com/developer/msbuild/2003");
+
+	    XmlNodeList itemGroups = doc.SelectNodes("/default:Project/default:ItemGroup", xmlnsManager);
+	    XmlNode include = null;
+	    foreach(XmlNode node in itemGroups) {
+		if (node.FirstChild.Name == "Compile") {
+		    include = node;
+		}
+	    }
+		
+	    if (include == null) {
+		Console.Out.WriteLine("ERROR: ItemGroup node with Compile nodes not found");
+	    	return false;
+	    }
+		
+	    Boolean updated = false;
+	    foreach(String filename in files) {
+		FileInfo file = new FileInfo(filename);
+
+		String path = file.FullName.Substring(project.DirectoryName.Length + 1);
+		String xpath = "//default:ItemGroup/default:Compile[@Include='" + path + "']";
+
+		XmlNode node = doc.SelectSingleNode(xpath, xmlnsManager);
+		if (node == null) {
+		    Console.Out.WriteLine("Adding " + path + " to " + project.FullName);
+
+		    //<Compile Include="BusinessLogic\Address.cs">
+		    //  <SubType>Code</SubType>
+		    //</Compile>
+
+		    XmlElement element = doc.CreateElement("Compile", "http://schemas.microsoft.com/developer/msbuild/2003");
+		    XmlAttribute attribute = doc.CreateAttribute("Include");
+		    attribute.Value = path;
+		    element.Attributes.Append(attribute);
+		
+		    XmlElement subType = doc.CreateElement("SubType", "http://schemas.microsoft.com/developer/msbuild/2003");
+		    subType.InnerText = "Code";
+		    element.AppendChild(subType);
+
+		    include.AppendChild(element);
+		    updated = true;
+		}
+	    }
+	    return updated;
+	}
+    	
+    	
     }
 }
