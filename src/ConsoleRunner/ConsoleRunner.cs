46abd7ab5a816b59a6f312ea9b3eebf6805dc836
paulf <none@none>
2011-02-22 07:51:40 +0000

    Added support for NDesk.Option in Spring2.Core for argument parsing to DataTierGenerator.exe and UpdateProjectFiles.exe
    WARNING: Anyone updating to this version (or higher) of the DTG will need to adjust their calls to DataTierGenerator and UpdateProjectFiles. You can use DataTierGenerator.exe -h or UpdateProjectFiles.exe -h to get a usage example and a list of valid options.

--- src/ConsoleRunner/ConsoleRunner.cs
+++ src/ConsoleRunner/ConsoleRunner.cs
@@ -6,6 +6,7 @@ using System.Xml.Schema;
 using System.Reflection;
 
 using Spring2.Core.Xml;
+using Spring2.Core.Util;
 
 using Spring2.DataTierGenerator.Generator;
 
@@ -15,38 +16,39 @@ namespace Spring2.DataTierGenerator.ConsoleRunner {
     /// Console runner
     /// </summary>
     class ConsoleRunner {
+	private static Boolean verbose = false;
+	private static Boolean showHelp = false;
+	private static String filename = String.Empty;
+	private static OptionSet os = null;
 
 	/// <summary>
 	/// Assembly entry point
 	/// </summary>
 	[STAThread]
 	static void Main(string[] args) {
+	    ParseArguments(args);
+	    Boolean valid = Validate();
 
-            Boolean verbose = false;
-
-	    if (args.Length==1) {
-		Generate(args[args.Length - 1],verbose);
-            } else if (args.Length == 2) {
-                if(args[0] == "-v") {
-                    verbose = true;
-                    Generate(args[args.Length - 1], verbose);
-                }
-            }  else {
-		Console.Out.WriteLine("usage: DataTierGenerator.exe <xml config filename>");
+	    if (showHelp) {
+		ShowHelp(os);
+	    }else if (valid) {
+		Generate(filename, verbose);
+		//Console.ReadKey();
+	    } else {
+		SuggestHelp();
 	    }
-            //Console.ReadKey();
 	}
 
-	private static void Generate(String filename,Boolean verbose) {
+	private static void Generate(String filename, Boolean verbose) {
 	    FileInfo file = new FileInfo(filename);
 	    if (!file.Exists) {
 		Console.Out.WriteLine("could not find config file: " + file.FullName);
 		return;
 	    }
 	    try {
-		Console.Out.WriteLine(String.Empty.PadLeft(40,'='));
+		Console.Out.WriteLine(String.Empty.PadLeft(40, '='));
 		Console.Out.WriteLine("Start :: " + DateTime.Now.ToString());
-		Console.Out.WriteLine(String.Empty.PadLeft(40,'='));
+		Console.Out.WriteLine(String.Empty.PadLeft(40, '='));
 
 		XmlDocument doc = new XmlDocument();
 		// while this might seem silly, extended ASCII character encoding does not happen if just the filename 
@@ -62,7 +64,7 @@ namespace Spring2.DataTierGenerator.ConsoleRunner {
 		System.Type clazz = System.Type.GetType(parserClassname, true);
 		Object o = System.Activator.CreateInstance(clazz);
 		if (o is IParser) {
-		    IParser parser = (IParser) o;
+		    IParser parser = (IParser)o;
 		    parser.Parse(filename);
 
 		    if (parser.IsValid) {
@@ -72,8 +74,8 @@ namespace Spring2.DataTierGenerator.ConsoleRunner {
 			    clazz = System.Type.GetType(parser.Generator, true);
 			    o = System.Activator.CreateInstance(clazz);
 			    if (o is IGenerator) {
-				generator = (IGenerator) o;
-			    } else  {
+				generator = (IGenerator)o;
+			    } else {
 				Console.Out.WriteLine("ERROR: class " + parser.Generator + " does not support IGenerator interface.\n");
 			    }
 			} catch (Exception ex) {
@@ -82,34 +84,34 @@ namespace Spring2.DataTierGenerator.ConsoleRunner {
 
 			// if the generator is not null, generate
 			if (generator != null) {
-                            if (verbose && generator is NVelocityGenerator) {
-                                ((NVelocityGenerator)generator).AttachToOutputEvent(ConsoleRunner.Output);
-                            }
+			    if (verbose && generator is NVelocityGenerator) {
+				((NVelocityGenerator)generator).AttachToOutputEvent(ConsoleRunner.Output);
+			    }
 			    generator.Generate(parser);
-			    foreach(String s in generator.Log) {
+			    foreach (String s in generator.Log) {
 				Console.Out.WriteLine(s);
 			    }
 			}
 		    } else {
 			Console.Out.WriteLine("ERROR: Parser found errors:");
-			foreach(String s in parser.Log) {
+			foreach (String s in parser.Log) {
 			    Console.Out.WriteLine(s);
 			}
 		    }
-		} else  {
+		} else {
 		    Console.Out.WriteLine("ERROR: class " + parserClassname + " does not support IParser interface.\n");
 		}
 
-		Console.Out.WriteLine(String.Empty.PadLeft(40,'='));
+		Console.Out.WriteLine(String.Empty.PadLeft(40, '='));
 		Console.Out.WriteLine("Done :: " + DateTime.Now.ToString());
-		Console.Out.WriteLine(String.Empty.PadLeft(40,'='));
+		Console.Out.WriteLine(String.Empty.PadLeft(40, '='));
 	    } catch (Exception ex) {
 		Console.Out.WriteLine("An error occurred while generating.\n\n" + ex.ToString());
 	    }
 	}
 
 	protected static XmlNode GetProcessingInstruction(XmlDocument doc, String pi) {
-	    foreach(XmlNode node in doc.ChildNodes) {
+	    foreach (XmlNode node in doc.ChildNodes) {
 		if (node.Name.Equals(pi)) {
 		    return node;
 		}
@@ -119,17 +121,58 @@ namespace Spring2.DataTierGenerator.ConsoleRunner {
 
 	protected static String GetProcessingInstructionAttribute(XmlNode node, String attribute) {
 	    String s = node == null ? String.Empty : node.Value;
-	    if (s != null && s.IndexOf(attribute + "=\"") >= 0) { 
+	    if (s != null && s.IndexOf(attribute + "=\"") >= 0) {
 		String value = s.Substring(s.IndexOf(attribute + "=\"") + attribute.Length + 2);
-		return value.Substring(0,value.IndexOf("\""));
+		return value.Substring(0, value.IndexOf("\""));
 	    }
 
 	    return null;
 	}
 
-        public static void Output(String s) {
-            Console.Out.WriteLine(s);
-        }
+	public static void Output(String s) {
+	    Console.Out.WriteLine(s);
+	}
 
+	static void ParseArguments(string[] args) {
+	    os = new OptionSet(){
+		{"v|verbose", "Increases debug verbosity", v => verbose = v != null},
+		{"h|help", "Show help message and exists", h => showHelp = h != null},
+		{"f|file=", "The config {FILE} to process", f => filename = f }
+	    };
+
+	    try {
+		os.Parse(args);
+	    } catch (OptionException e) {
+		Console.Write("DataTierGenerator: ");
+		Console.WriteLine(e.Message);
+		SuggestHelp();
+	    }
+	}
+
+	static Boolean Validate() {
+	    bool valid = true;
+
+	    if (string.IsNullOrEmpty(filename)) {
+		Console.WriteLine("Try `DataTierGenerator.exe --help' for more information.");
+		Console.ReadLine();
+		valid = false;
+	    }
+
+	    return valid;
+	}
+
+	static void ShowHelp(OptionSet p) {
+	    Console.WriteLine("Usage: DataTierGenerator.exe [OPTIONS]");
+	    Console.WriteLine("Options:");
+	    p.WriteOptionDescriptions(Console.Out);
+	    Console.WriteLine();
+	    Console.WriteLine("Deprecated Usage: DataTierGenerator.exe (optional)-v configFile");
+	    Console.ReadLine();
+	}
+
+	static void SuggestHelp() {
+	    Console.WriteLine("Try `DataTierGenerator.exe --help' for more information.");
+	    Console.ReadLine();
+	}
     }
 }
--- src/UpdateProjectFiles/UpdateProjectFiles.cs
+++ src/UpdateProjectFiles/UpdateProjectFiles.cs
@@ -1,46 +1,55 @@
 using System;
 using System.Collections.Specialized;
+using System.Collections.Generic;
 using System.IO;
 using System.Xml;
 
+using Spring2.Core.Util;
+
 namespace Spring2.DataTierGenerator.UpdateProjectFiles {
- 
+
     /// <summary>
     /// Updates a project with files not already included in project based on file pattern arguments
     /// </summary>
     public class UpdateProjectFiles {
-	
-    	/// <summary>
+	private static Boolean recurse = false;
+	private static Boolean showHelp = false;
+	private static String searchPattern = "*.cs";
+	private static List<string> directoriesToProcess = new List<string>();
+	private static List<string> filesToProcess = new List<string>();
+	private static FileInfo project = null;
+	private static OptionSet os = null;
+
+	private static List<string> files = new List<string>();
+
+	/// <summary>
 	/// The main entry point for the application.
 	/// </summary>
 	[STAThread]
 	static void Main(string[] args) {
-	    // assumes that the project filename is the first argument
-	    FileInfo project = new FileInfo(args[0]);
-	    if (!project.Exists) {
-		Console.Out.WriteLine("Project file could not be found: " + project.FullName);
-		return;
-	    }
+	    ParseArguments(args);
+	    Boolean valid = Validate();
 
-	    // the rest of the arguments could be files or directories
-	    StringCollection files = new StringCollection();
-	    for(Int32 i=1; i < args.Length; i++) {
-		String[] filenames = Directory.GetFiles(Environment.CurrentDirectory, args[i]);
-		files.AddRange(filenames);
+	    if (showHelp) {
+		ShowHelp(os);
+	    } else if (valid) {
+		ExecuteTask();
+	    } else {
+		SuggestHelp();
 	    }
-
-	    ExecuteTask(project, files);
 	}
 
-	private static void ExecuteTask(FileInfo project, StringCollection files) {
+	private static void ExecuteTask() {
+	    GetFiles();
+
 	    XmlDocument doc = new XmlDocument();
 	    doc.Load(project.FullName);
 
 	    Boolean updated = false;
 	    if (doc.DocumentElement.Name.Equals("VisualStudioProject")) {
-		updated = AddFilesVS2003(doc, files, project);
+		updated = AddFilesVS2003(doc);
 	    } else if (doc.DocumentElement.Name.Equals("Project")) {
-		updated = AddFilesVS2005(doc, files, project);
+		updated = AddFilesVS2005(doc);
 	    } else {
 		Console.Out.WriteLine("ERROR: Unknown project file version");
 	    }
@@ -53,16 +62,16 @@ namespace Spring2.DataTierGenerator.UpdateProjectFiles {
 	    }
 	}
 
-	private static bool AddFilesVS2003(XmlDocument doc, StringCollection files, FileInfo project) {
+	private static bool AddFilesVS2003(XmlDocument doc) {
 	    XmlNode include = doc.SelectSingleNode("/VisualStudioProject/CSHARP/Files/Include");
-		
+
 	    if (include == null) {
 		Console.Out.WriteLine("ERROR: Include node not found");
 		return false;
 	    }
 
 	    Boolean updated = false;
-	    foreach(String filename in files) {
+	    foreach (String filename in files) {
 		FileInfo file = new FileInfo(filename);
 
 		String path = file.FullName.Substring(project.DirectoryName.Length + 1);
@@ -97,8 +106,8 @@ namespace Spring2.DataTierGenerator.UpdateProjectFiles {
 	    }
 	    return updated;
 	}
-    	
-	private static bool AddFilesVS2005(XmlDocument doc, StringCollection files, FileInfo project) {
+
+	private static bool AddFilesVS2005(XmlDocument doc) {
 	    //Instantiate an XmlNamespaceManager object. 
 	    XmlNamespaceManager xmlnsManager = new XmlNamespaceManager(doc.NameTable);
 
@@ -107,19 +116,19 @@ namespace Spring2.DataTierGenerator.UpdateProjectFiles {
 
 	    XmlNodeList itemGroups = doc.SelectNodes("/default:Project/default:ItemGroup", xmlnsManager);
 	    XmlNode include = null;
-	    foreach(XmlNode node in itemGroups) {
+	    foreach (XmlNode node in itemGroups) {
 		if (node.FirstChild.Name == "Compile") {
 		    include = node;
 		}
 	    }
-		
+
 	    if (include == null) {
 		Console.Out.WriteLine("ERROR: ItemGroup node with Compile nodes not found");
-	    	return false;
+		return false;
 	    }
-		
+
 	    Boolean updated = false;
-	    foreach(String filename in files) {
+	    foreach (String filename in files) {
 		FileInfo file = new FileInfo(filename);
 
 		String path = file.FullName.Substring(project.DirectoryName.Length + 1);
@@ -137,7 +146,7 @@ namespace Spring2.DataTierGenerator.UpdateProjectFiles {
 		    XmlAttribute attribute = doc.CreateAttribute("Include");
 		    attribute.Value = path;
 		    element.Attributes.Append(attribute);
-		
+
 		    XmlElement subType = doc.CreateElement("SubType", "http://schemas.microsoft.com/developer/msbuild/2003");
 		    subType.InnerText = "Code";
 		    element.AppendChild(subType);
@@ -147,6 +156,76 @@ namespace Spring2.DataTierGenerator.UpdateProjectFiles {
 		}
 	    }
 	    return updated;
+	}
+
+	private static void GetFiles() {
+	    foreach (String directory in directoriesToProcess) {
+		if (recurse) {
+		    AddFilesRecursively(directory);
+		} else {
+		    AddFiles(directory);
+		}
+	    }
+
+	    foreach (String file in filesToProcess) {
+		files.Add(file);
+	    }
+	}
+
+	private static void AddFilesRecursively(String directory) {
+	    AddFiles(directory);
+	    List<String> subDirectories = new List<String>(Directory.GetDirectories(directory));
+	    foreach (String subDir in subDirectories) {
+		AddFilesRecursively(subDir);
+	    }
+
+	}
+
+	private static void AddFiles(String directory) {
+	    files.AddRange(Directory.GetFiles(directory, searchPattern));
+	}
+
+	private static void ParseArguments(string[] args) {
+	    var os = new OptionSet(){
+		{"p|project=", "The fileName of the project", p => project = new FileInfo(p) },
+		{"d|directories=", "Comma separated directories to process", d => directoriesToProcess.AddRange(d.Split(',')) },
+		{"f|files=", "Comma separated files to process", f => filesToProcess.AddRange(f.Split(',')) },
+		{"s|searchPattern=", "Allows you to specify which files to process. Default is *.cs", s => searchPattern = s },
+		{"h|help", "Show help message and exists", h => showHelp = h != null },
+		{"r|recurse", "Recurse on all given directories", r => recurse = r != null }
+	    };
+
+	    try {
+		os.Parse(args);
+	    } catch (OptionException e) {
+		Console.Write("UpdateProjectFiles: ");
+		Console.WriteLine(e.Message);
+		SuggestHelp();
+	    }
+	}
+
+	static Boolean Validate() {
+	    bool valid = true;
+
+	    if (!project.Exists) {
+		Console.Out.WriteLine("Project file could not be found: " + project.FullName);
+		valid = false;
+	    }
+
+	    return valid;
+	}
+
+	static void ShowHelp(OptionSet p) {
+	    Console.WriteLine("Usage: DataTierGenerator.exe [OPTIONS]");
+	    Console.WriteLine("Options:");
+	    p.WriteOptionDescriptions(Console.Out);
+	    Console.WriteLine();
+	    Console.WriteLine("Deprecated Usage: DataTierGenerator.exe (optional)-v configFile");
+	}
+
+	static void SuggestHelp() {
+	    Console.WriteLine("Try `UpdateProjectFiles.exe --help' for more information.");
+	    Console.ReadLine();
 	}
     }
 }
