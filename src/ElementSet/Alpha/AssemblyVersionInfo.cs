5734600ce2bfd468a24a816e1a534aa50290dc86
rolandk <none@none>
2011-03-14 21:56:14 +0000

    Updated the nunit.framework.dll to 2.5.7 and updated the AssemblyVersionInfo.cs to be the same as that from src directory.

--- src/ElementSet/Alpha/AssemblyVersionInfo.cs
+++ src/ElementSet/Alpha/AssemblyVersionInfo.cs
@@ -1,12 +1,15 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
 //
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersion("1.2.0.2343")]
